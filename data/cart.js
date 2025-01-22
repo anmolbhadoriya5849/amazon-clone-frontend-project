141bac6ff878c1a86603107ea08c3e23c3d1d967
anmolbhadoriya5849 <technoanmol5849@gmail.com>
2025-01-22 23:17:13 +0530

    created js for checkout page and make it interactive

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,24 @@
-export const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart = [
+        {
+            productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity : 2,
+            deliveryOptionId : '1'
+        },
+        {
+            productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity : 1,
+            deliveryOptionId : '2'
+        }
+    ];
+}
+
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -17,7 +37,23 @@ export function addToCart(productId){
     else{
         cart.push({
             productId : productId,
-            quantity : Number(quantity)
+            quantity : Number(quantity),
+            deliveryOptionId : '1'
         });
     }
+
+    saveToStorage();
 }
+
+export function deleteFromCart(productId){
+    const newArr = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId!==productId){
+            newArr.push(cartItem);
+        }
+    });
+
+    cart = newArr;
+    saveToStorage();
+};
